786bf3883e84d0b45ba1ba6c6ebd9bff52913ad0
misadowsk <michalsad.protondynamic@gmail.com>
2024-04-09 13:19:04 +0200

    #3 changed users to dict from list to easier call data

--- App/main.py
+++ App/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from insert_data import get_data
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -11,4 +12,6 @@ def home():
 
 if __name__ == '__main__':
     users = get_data()
+    users = [OrderedDict(zip(['customer_id', 'name', 'last_name',
+                         'email', 'phone_number'], user)) for user in users]
     app.run(debug=True)
