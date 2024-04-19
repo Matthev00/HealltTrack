e0321c3be48be9f7e826ada481be30dbec38ca81
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-04-19 10:54:09 +0100

    added add_meal_food func

--- backend/DB/DBHandler.py
+++ backend/DB/DBHandler.py
@@ -1,6 +1,7 @@
 from typing import Dict
 import json
 import datetime
+import oracledb
 
 from backend.DB.DBConnector import DBConnector
 
@@ -32,22 +33,53 @@ class DBHandler:
                 ]
             )
 
-    def add_meal_food(self, dict: Dict) -> str:
-        date_time = self.convert_date_format(dict["date_time"])
-        food_id = dict["food_id"]
-        quantity = dict["quantity"]
-        meal_type = dict["meal_type"]
+    def add_meal_food(self, meal_data: Dict) -> str:
+        date_time = meal_data["date_time"]
+        food_id = meal_data["food_id"]
+        quantity = meal_data["quantity"]
+        meal_type = meal_data["meal_type"]
+        user_id = meal_data["user_id"]
+
         with self.connection.cursor() as cursor:
             cursor.execute(
-                "SELECT meal_entry_id, date_time FROM meal_entry WHERE date_time = :date_time",
-                date_time=date_time,
+                "SELECT meal_entry_id FROM meal_entry JOIN meal ON meal_entry.meal_meal_id = meal.meal_id WHERE TRUNC(meal_entry.date_time) = TRUNC(TO_DATE(:date_time, 'DD-MM-YYYY')  AND meal.meal_type_meal_type_id = :meal_type",
+                {"date_time": date_time, "meal_type": meal_type},
             )
-            
+            result = cursor.fetchone()
 
+            if result:
+                meal_id = result[0]
+            else:
+                cursor.execute("SELECT MAX(meal_id) FROM meal")
+                result = cursor.fetchone()
+                if result[0] is None:
+                    meal_id = 1
+                else:
+                    meal_id = result[0] + 1
+                cursor.execute(
+                    "INSERT INTO meal (water_consumption, calories, proteins, fats, carbohydrates, meal_type_meal_type_id, meal_id) VALUES (0, 0, 0, 0, 0, :meal_type, :meal_id)",
+                    {"meal_type": meal_type, "meal_id": meal_id},
+                )
+                cursor.execute("SELECT MAX(meal_entry_id) FROM meal_entry")
+                result = cursor.fetchone()
+                if result[0] is None:
+                    meal_entry_id = 1
+                else:
+                    meal_entry_id = result[0] + 1
+                cursor.execute(
+                    "INSERT INTO meal_entry (meal_entry_id, user_user_id, meal_meal_id, date_time) VALUES (:meal_entry_id, :user_id, :meal_id, :date_time)",
+                    {
+                        "meal_entr_id": meal_entry_id,
+                        "user_id": user_id,
+                        "meal_id": meal_id,
+                        "date_time": date_time,
+                    },
+                )
 
-    def convert_date_format(date_str: str) -> str:
-        date_obj = datetime.strptime(date_str, '%d-%m-%Y')
-        return date_obj.strftime('%Y-%m-%d')
+            cursor.execute(
+                "INSERT INTO meal_food (meal_id, food_id, quantity) VALUES (:meal_id, :food_id, :quantity)",
+                {"meal_id": meal_id, "food_id": food_id, "quantity": quantity},
+            )
 
     def close(self):
         self.db_connector.close()
