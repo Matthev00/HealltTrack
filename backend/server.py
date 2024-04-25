6068b30495e00768503099a032a90215ca967f95
misadowsk <michalsad.protondynamic@gmail.com>
2024-04-25 11:01:50 +0200

    fixed dispalying meals from day

--- backend/server.py
+++ backend/server.py
@@ -16,12 +16,13 @@ def return_home():
     return jsonify(food_list)  # Convert the food_list to JSON format
 
 
-@app.route('/<date>', methods=['GET'])
+@app.route('/popup_food/<date>', methods=['GET'])
 def display_data_in_specific_day(date):
     with open("backend/DB/wallet_credentials.json") as f:
         wallet_credentials = json.load(f)
     db = DBHandler(wallet_credentials=wallet_credentials)
-    day_history = db.get_day_history(date)
+    date_data = {'date': date, 'user_id': 1}
+    day_history = db.get_day_history(date_data)
     return jsonify(day_history)
 
 
