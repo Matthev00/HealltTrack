6e6cb0771ba34ec680add1b489102781bab504e2
misadowsk <michalsad.protondynamic@gmail.com>
2024-04-25 10:46:04 +0200

    added passing information about specific day

--- backend/server.py
+++ backend/server.py
@@ -16,13 +16,13 @@ def return_home():
     return jsonify(food_list)  # Convert the food_list to JSON format
 
 
-@app.route('/{date}', methods=['GET'])
-def display_data_in_specific_day():
+@app.route('/<date>', methods=['GET'])
+def display_data_in_specific_day(date):
     with open("backend/DB/wallet_credentials.json") as f:
         wallet_credentials = json.load(f)
     db = DBHandler(wallet_credentials=wallet_credentials)
-    food_list = db.get_food_list()
-    return jsonify(food_list)
+    day_history = db.get_day_history(date)
+    return jsonify(day_history)
 
 
 @app.route('/popup_food_out', methods=['POST'])
