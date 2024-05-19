cb9ff02620950ca2e9928b99838b4471f0387a6e
BartłomiejPuciłowski <pucilowskibartlomiej@gmail.com>
2024-05-19 12:07:48 +0200

    Add chart of day makros, it needed a package.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,56 @@
 "use client"
 
 import MainPageContext from "@/components/store/MainPageContext";
-import { useContext } from "react";
+import { macros } from "@/types";
+import { fetchMacrosFromDay } from "@/utils";
+import { useContext, useEffect, useState } from "react";
+import { PieChart } from 'react-minimal-pie-chart';
+// ... other imports
 
 export default function MainPage() {
   const mainPageCtx = useContext(MainPageContext);
+  const [dayMacros, setDayMacros] = useState<macros>({
+    kcal: 0,
+    proteins: 0,
+    fats: 0,
+    carbs: 0,
+    water: 0,
+  });
+
+  useEffect(() => {
+    fetchMacrosFromDay(1, mainPageCtx.actualDate).then(setDayMacros);
+  }, [mainPageCtx.actualDate]);
+
+  // Prepare data for the chart
+  const chartData = ['proteins', 'fats', 'carbs'].map((key, i) => ({
+    title: key,
+    value: dayMacros[key],
+    color: `hsl(${i * (360 / 3)}, 70%, 50%)`,
+  }));
+
 
   return (
     <div className="w-full h-full">
       <div className="flex w-full h-[15%] justify-center pt-6">
         <div className="flex-1 border-r text-center">
           <span className="">{mainPageCtx.actualDate}</span>
+          <div>Kcal: {dayMacros.kcal}</div>
+          <div>Water: {dayMacros.water} ml</div>
+        </div>
+      </div>
+      <div className="flex w-full h-[85%] justify-center pt-6">
+        <div style={{ width: '70%', height: '70%', marginLeft: '-50%' }}>
+        <PieChart 
+          data={chartData} 
+          label={({ dataEntry }) => `${dataEntry.title}: ${dataEntry.value}`}
+          labelStyle={{ fontSize: '7px' }} // Increase this value to make the font bigger
+        />
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
