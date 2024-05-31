0ad802d3103c4e6efe88bb67a54388077bca4a7a
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-05-31 18:12:22 +0100

    Refactor SQL query to retrieve body measurements for a specific day in correct format

--- backend/DB/queries/get_body_measurement_day.sql
+++ backend/DB/queries/get_body_measurement_day.sql
@@ -1,4 +1,4 @@
-SELECT TO_CHAR(bme.date_time, 'DD-MM-YYYY-HH24') AS date_time, bme.weight
+SELECT TO_CHAR(bme.date_time, 'DD-MM-YYYY') AS date_time, bme.weight
 FROM body_measurement_entry bme
 WHERE bme.user_user_id = :user_id
     AND TRUNC(bme.date_time) <= TRUNC(TO_DATE(:query_date, 'DD-MM-YYYY'))
